affc77d1ec6ceea2a336446cc07bded5db2346f7
Cody Johnson <codymj16@aol.com>
2020-10-23 13:10:30 -0600

    initial push

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    // moment variable
+    var hr = moment().hours();
+
+    // current date
+    function currentDate() {
+        $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    };
+
+    // color Event Rows
+    function colorMeJack() {
+        $("input").each(function() {
+            var rHour = $(this).attr("id");
+            var rNum = parseInt(rHour);
+            if (rNum === hr) {
+                $(this).addClass("present");
+            } else if (rNum > hr) {
+                $(this).addClass("future");
+            } else {
+                $(this).addClass("past");
+            };
+        });
+    };
+
+    // save events to local storage via button
+    $(".saveBtn").click(function() {
+        var imputs = $(this).siblings(".event").val();
+        var imputLoc = $(this).siblings(".event").attr("id");
+        localStorage.setItem(imputs,imputLoc);
+    })
+
+    // retrieving stored imputs for later use
+    function retrieveSchedule() {
+        $(".event").each(function() {
+            var eventId = $(this).attr("id");
+            $(this).val(localStorage.getItem(eventId))
+        });
+    };
+
+    setInterval(currentDate,1000);
+    colorMeJack();
+    setInterval(colorMeJack,1000);
+    retrieveSchedule();
+
+})
+
