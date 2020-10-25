77329c9f833f70f85140404fe026d4d3b1c6c7a0
Cody Johnson <codymj16@aol.com>
2020-10-25 12:22:59 -0600

    removed console.logs and setInterval function

--- script.js
+++ script.js
@@ -9,23 +9,14 @@ $(document).ready(function() {
 
     // color Event Rows
     function colorMeJack() {
-        
         $("input").each(function() {
-            console.log(this)
-            console.log(hr)
             var rHour = $(this).attr("id");
             var rNum = parseInt(rHour);
-            console.log("thisisrHour" + rHour)
-            console.log("this is rnum "+ rNum)
             if (rNum === hr) {
-                console.log("if")
-            
                 $(this).addClass("present");
             } else if (rNum > hr) {
-                console.log("else if")
                 $(this).addClass("future");
             } else {
-                console.log("else")
                 $(this).addClass("past");
             };
         });
@@ -35,21 +26,22 @@ $(document).ready(function() {
     $(".saveBtn").click(function() {
         var imputs = $(this).siblings(".event").val();
         var imputLoc = $(this).siblings(".event").attr("id");
-        localStorage.setItem(imputs,imputLoc);
+        localStorage.setItem(imputs, imputLoc);
     })
 
     // retrieving stored imputs for later use
     function retrieveSchedule() {
-        $(".event").each(function() {
+        $("event").each(function() {
             var eventId = $(this).attr("id");
             $(this).val(localStorage.getItem(eventId))
         });
+        console.log(retrieveSchedule);
     };
 
     setInterval(currentDate,1000);
     colorMeJack();
-    setInterval(colorMeJack,1000);
     retrieveSchedule();
+    
 
 })
 
